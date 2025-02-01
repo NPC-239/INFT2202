764693cac323762815d8212d88c81c77fd30b124
Mateo Valles <114031093+NPC-239@users.noreply.github.com>
2025-01-31 21:07:23 -0500

    import reference fix

--- test1/src/client/js/index.js
+++ test1/src/client/js/index.js
@@ -1,5 +1,5 @@
 // import the movies array from the supplied data file.
-import { movies } from './movies.js';
+import { movies } from './data/movies.js';
 
 // write the array to the console, so you can see that they are loading properly
 /* call insertMoviesIntoTable, 
